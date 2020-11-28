0066b168f5238fcc0fa6334fc87efd2e9770f601
manuelruizredondo <manuelruizredondo@gmail.com>
2020-11-28 11:24:02 +0100

    limpieza de js no usados

--- build/assets/js/scripts.js
+++ build/assets/js/scripts.js
@@ -5,44 +5,6 @@ $(document).ready(function () {
     $("html").toggleClass("noscroll-menu ");
   });
 
-  ("use strict");
-
-  TweenLite.set('#frontal1inner', { left:"50%" , top:"0", xPercent:-50 ,opacity:1 });
-  TweenLite.set('#frontal3inner', { left:"50%" , top:"0", xPercent:-50 ,opacity:0 });
-
-  TweenLite.set('#frontal3inner', { left:"50%" , top:"0", xPercent:-50 ,opacity:0 });
-
-
-  $('#frontal1').click(function () {
-    if (!TweenMax.isTweening('#frontal1inner')) {   
-
-        TweenLite.to('#frontal1inner', .3, { left:"50%" , top:"0", xPercent:-50 ,opacity:1 });
-        TweenLite.to('#frontal2inner', .3, { left:"50%",  top:"0", xPercent:-50 , opacity:0 });
-        TweenLite.to('#frontal3inner', .3, { left:"50%",  top:"0", xPercent:-50 , opacity:0 });
-    }
-    return false;
-  });
-  8
-  
-  $('#frontal2').click(function () {
-    if (!TweenMax.isTweening('#frontal2inner')) {
-        TweenLite.to('#frontal2inner', .3,{ left:"50%" , top:"0", xPercent:-50 ,opacity:1 });
-        TweenLite.to('#frontal1inner', .3, { left:"50%", top:"0", xPercent:-50 ,opacity:0 });
-        TweenLite.to('#frontal3inner', .3, { left:"50%", top:"0", xPercent:-50 ,opacity:0 });
-    }
-    return false;
-  });
-  
-  
-  $('#frontal3').click(function () {
-    if (!TweenMax.isTweening('#frontal3inner')) {
-        TweenLite.to('#frontal3inner', .3,{ left:"50%" , top:"0", xPercent:-50 , opacity:1 });
-        TweenLite.to('#frontal1inner', .3, { left:"50%", top:"0", xPercent:-50 , opacity:0 });
-        TweenLite.to('#frontal2inner', .3, { left:"50%", top:"0", xPercent:-50 , opacity:0 });
-    }
-    return false;
-  });
-
 
 });
 
